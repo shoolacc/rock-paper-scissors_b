fb50645f898359dfc1a0b30152334ba4030b057f
shoolacc <123706564+shoolacc@users.noreply.github.com>
2023-02-02 11:19:31 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,90 @@
+input.onButtonPressed(Button.A, function () {
+    computer_choice = randint(0, 2)
+    if (computer_choice == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+    if (computer_choice == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (computer_choice == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    game.setScore(0)
+})
+input.onButtonPressed(Button.B, function () {
+    if (computer_choice == player1_choice) {
+        basic.showString("Draw!")
+    } else {
+        if (computer_choice < player1_choice) {
+            if (computer_choice == 2 && player1_choice == 0) {
+                basic.showString("Computer Wins")
+                game.setScore(1)
+                basic.showString("" + (game.score()))
+            } else {
+                basic.showString("Player Wins!")
+                game.setScore(1)
+                basic.showString("" + (game.score()))
+            }
+        } else {
+            if (player1_choice == 2 && computer_choice == 0) {
+                basic.showString("Computer Loses")
+            } else {
+                basic.showString("Player Loses")
+            }
+        }
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    player1_choice = randint(0, 2)
+    if (player1_choice == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+    if (player1_choice == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (player1_choice == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+let player1_choice = 0
+let computer_choice = 0
+basic.showString("Shake to play")
+game.setScore(0)
